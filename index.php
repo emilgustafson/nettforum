d8302e6873ea87298e8670ceb79fbb963efd0047
emilgustafson <emilio.gustavio@gmail.com>
2019-03-12 00:58:19 +0100

    startet på nytt
    Har laget logo til headeren. Helt nytt layout. Ny fargepalett

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="stilarkindex.css">
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<div class="wrapper">
+
+		
+		
+	</div>
+
+</body>
+</html>
